0bd7cde2c83d7c44e9c134728efba36ba986b490
Kirill Zykov <kiryushaz.gaming@gmail.com>
2023-10-31 12:15:23 +0600

    change background color

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.myapplication.screens.MainScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF050B18)
                 ) {
                     MainScreen()
                 }
